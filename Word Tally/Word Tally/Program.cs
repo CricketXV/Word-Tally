1325dcb390374dee8f4d03dfeb600cca0e6e2c5d
CricketXV <33155889+CricketXV@users.noreply.github.com>
2018-01-10 10:05:34 -0500

    half way
    half way done just needs some debug

--- Word Tally/Word Tally/Program.cs
+++ Word Tally/Word Tally/Program.cs
@@ -9,27 +9,28 @@ namespace Word_Tally
     {
         static void Main(string[] args)
         {
-            StreamWriter sw = new StreamWriter("tallies.txt");
-            string input = File.ReadAllText("passage.txt");
-            input = input.ToLower();
-            string passage = "";
+			StreamReader sr = new StreamReader ("passage.txt");
+			StreamWriter sw = new StreamWriter("tallies.txt");
 
-            foreach (char c in input) {
-                if (c >= 97 && c<= 122 || c == 32)
-                {
-                    passage += c;
-                     
-                }
-            }
-            string[] words = passage.Split(' ');
-            SortedDictionary<string, int> dictionary = new SortedDictionary<string, int>();
-            foreach ()
-            {
-                dictionary.Add("cool", 1);
-                sw.WriteLine("{0}", pair.Key);
-            }
-            
-            sw.Close();
+			while (!sr.EndOfStream)
+			{
+				{
+					string currentLine = sr.ReadLine ();
+					currentLine.ToLower ();
+					string stripped = ("");
+					foreach (char c in currentLine)
+					{
+						if(c > 96 && c < 123 || c == 32 )
+						{
+							stripped = string.Join(stripped, c);
+
+						}
+						Console.Write (stripped);
+					}
+					string[] words = currentLine.Split (' ');
+
+				}
+			}  
         }
     }
 }
