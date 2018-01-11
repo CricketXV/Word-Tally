41d0be83a27ee39150e74b31461b2222f6dd3fb8
CricketXV <33155889+CricketXV@users.noreply.github.com>
2018-01-11 10:07:23 -0500

    almost done

--- Word Tally/Word Tally/Program.cs
+++ Word Tally/Word Tally/Program.cs
@@ -5,32 +5,32 @@ using System.Collections.Generic;
 
 namespace Word_Tally
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+	 class Program
+	 {
+	   static void Main(string[] args)
+	   {
 			StreamReader sr = new StreamReader ("passage.txt");
 			StreamWriter sw = new StreamWriter("tallies.txt");
 
 			while (!sr.EndOfStream)
 			{
+				string currentLine = sr.ReadLine ();
+				currentLine = currentLine.ToLower ();
+				string stripped = "";
+
+			
+				foreach (char c in currentLine) 
 				{
-					string currentLine = sr.ReadLine ();
-					currentLine.ToLower ();
-					string stripped = ("");
-					foreach (char c in currentLine)
+					if (c >= 97 && c <= 122 || c == 32) 
 					{
-						if(c > 96 && c < 123 || c == 32 )
-						{
-							stripped = string.Join(stripped, c);
-
-						}
-						Console.Write (stripped);
+						stripped += c;													
 					}
-					string[] words = currentLine.Split (' ');
-
 				}
+				String[] words = stripped.Split (' ');
+				SortedDictionary <string, int> dictionary = new SortedDictionary <string, int> ();
+
+
 			}  
-        }
-    }
+	   }
+	}
 }
